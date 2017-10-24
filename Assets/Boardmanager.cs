2b2d2b6a53183ed16c672e1bae0f040337539bdc
Axel <haxelegide@yahoo.fr>
2017-10-24 14:30:42 -0400

    Board made

---
+++ Assets/Boardmanager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boardmanager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
